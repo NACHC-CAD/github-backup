31257f2f3a0bf43fa2284a6c603f9660a36cead7
NACHC-CAD <greshje.nachc@gmail.com>
2022-12-19 20:24:35 -0500

    successfully doing clone

--- src/main/java/org/nachc/tools/githubbackup/main/GitHubBackupMain.java
+++ src/main/java/org/nachc/tools/githubbackup/main/GitHubBackupMain.java
@@ -1,5 +1,25 @@
 package org.nachc.tools.githubbackup.main;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.nachc.tools.githubbackup.util.gh.GetReposForUser;
+import org.yaorma.util.time.TimeUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class GitHubBackupMain {
 
+	public static void main(String[] args) {
+		String today = TimeUtil.format(TimeUtil.getNow(), "yyyy_MM_dd");
+		log.info("Today: " + today);
+		List<String> repos = GetReposForUser.exec();
+		for(String repo : repos) {
+			log.info("\tCloning repo: " + repo);
+//			Clone clone = new Clone();
+//			clone.exec(repo, null);
+		}
+	}
+	
 }
